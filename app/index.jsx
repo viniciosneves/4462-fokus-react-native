c176d8443700fc22fdfecc1fbfe1e3996143bf4e
Vinicios Neves <vinicios.neves@mac>
2025-01-20 19:42:09 +0000

    video 3.3

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import { Image, StyleSheet, Text, View, Pressable } from "react-native";
 
+const pomodoro = [
+  {
+    id: 'focus',
+    initialValue: 25,
+    image: require('./pomodoro.png')
+  },
+  {
+    id: 'short',
+    initialValue: 5,
+    image: require('./short.png')
+  },
+  {
+    id: 'long',
+    initialValue: 15,
+    image: require('./long.png')
+  },
+]
+
 export default function Index() {
+
+  const [timerType, setTimerType] = useState(pomodoro[0])
+
   return (
     <View
       style={styles.container}
     >
-      <Image source={require('./pomodoro.png')}/>
+      <Image source={timerType.image}/>
       <View style={styles.actions}>
         <View style={styles.context}>
           <Pressable style={styles.contextButtonActive}>
@@ -25,7 +47,7 @@ export default function Index() {
           </Pressable>
         </View>
         <Text style={styles.timer}>
-          25:00
+          { new Date(timerType.initialValue * 1000).toLocaleTimeString('pt-BR', { minute: '2-digit', second: '2-digit' }) }
         </Text>
         <Pressable style={styles.button}>
           <Text style={styles.buttonText}>
