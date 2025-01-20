228f4fb9fe7619b74b0e86e9af30f6a891adb6d2
Vinicios Neves <vinicios.neves@mac>
2025-01-20 21:34:00 +0000

    video 5.3

--- app/index.jsx
+++ app/index.jsx
@@ -3,23 +3,24 @@ import { Image, StyleSheet, Text, View, Pressable } from "react-native";
 import { FokusButton } from "../components/FokusButton";
 import { ActionButton } from "../components/ActionButton";
 import { Timer } from "../components/Timer";
+import { IconPause, IconPlay } from "../components/Icons";
 
 const pomodoro = [
   {
     id: 'focus',
-    initialValue: 25,
+    initialValue: 25 * 60,
     image: require('./pomodoro.png'),
     display: 'Foco'
   },
   {
     id: 'short',
-    initialValue: 5,
+    initialValue: 5 * 60,
     image: require('./short.png'),
     display: 'Pausa curta'
   },
   {
     id: 'long',
-    initialValue: 15,
+    initialValue: 15 * 60,
     image: require('./long.png'),
     display: 'Pausa longa'
   },
@@ -63,7 +64,6 @@ export default function Index() {
         }
         return oldState - 1
       })
-      console.log('timer rolando')
     }, 1000)
     timerRef.current = id
   }
@@ -87,6 +87,7 @@ export default function Index() {
         <Timer totalSeconds={seconds} />
         <FokusButton 
           title={timerRunning ? 'Pausar' : 'Começar'}
+          icon={timerRunning ? <IconPause /> : <IconPlay />}
           onPress={toggleTimer}
         />
       </View>
--- components/FokusButton/index.jsx
+++ components/FokusButton/index.jsx
@@ -1,8 +1,9 @@
 import { Pressable, StyleSheet, Text } from "react-native"
 
-export const FokusButton = ({ onPress, title }) => {
+export const FokusButton = ({ onPress, title, icon }) => {
     return (
         <Pressable style={styles.button} onPress={onPress}>
+            {icon}
             <Text style={styles.buttonText}>
                 {title}
             </Text>
@@ -14,7 +15,11 @@ const styles = StyleSheet.create({
   button: {
     backgroundColor: '#B872FF',
     borderRadius: 32,
-    padding: 8
+    padding: 8,
+    flexDirection: 'row',
+    gap: 12,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   buttonText: {
     textAlign: 'center',
