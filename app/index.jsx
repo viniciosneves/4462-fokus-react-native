17b84e09fe0bc6203bce524cf8aa4c7b85987b28
Vinicios Neves <vinicios.neves@mac>
2025-01-20 19:08:34 +0000

    video 2.3

--- app/index.jsx
+++ app/index.jsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, View } from "react-native";
+import { Image, StyleSheet, Text, View, Pressable } from "react-native";
 
 export default function Index() {
   return (
@@ -6,7 +6,24 @@ export default function Index() {
       style={styles.container}
     >
       <Image source={require('./pomodoro.png')}/>
-      <View style={styles.actions}/>
+      <View style={styles.actions}>
+        <Text style={styles.timer}>
+          25:00
+        </Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>
+            Começar
+          </Text>
+        </Pressable>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Projeto fictício e sem fins comerciais.
+        </Text>
+        <Text style={styles.footerText}>
+          Desenvolvido por Alura. 
+        </Text>
+      </View>
     </View>
   );
 }
@@ -26,6 +43,31 @@ const styles = StyleSheet.create({
     width: '80%',
     borderRadius: 32,
     borderWidth: 2,
-    borderColor: '#144480'
+    borderColor: '#144480',
+    gap: 32
+  },
+  timer: {
+    fontSize: 54,
+    color: '#FFF',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  button: {
+    backgroundColor: '#B872FF',
+    borderRadius: 32,
+    padding: 8
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: '#021123',
+    fontSize: 18
+  },
+  footer: {
+    width: '80%',
+  },
+  footerText: {
+    textAlign: 'center',
+    color: '#98A0A8',
+    fontSize: 12.5
   }
 })
